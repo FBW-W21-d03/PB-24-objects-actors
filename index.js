bbfbe31c1e8cec41db5dcfc4ea9f6ae51dc3ac86
Mak513 <osei.makangu@hotmail.com>
2021-12-06 11:31:01 +0100

    osei

---
+++ index.js
@@ -0,0 +1,24 @@
+const WashingtonJr = {
+    Geburtsjahr: 1954,
+    Wohnort: "USA",
+    Geschlecht: "männlich",
+    LetzterGespielterFilm: "A Journal for Jordan"
+};
+
+const Jackson = {
+    Geburtsjahr: 1948,
+    Wohnort: "USA",
+    Geschlecht: "männlich",
+    LetzterGespielterFilm: "The Marvels"
+};
+
+const Johansson = {
+    Geburtsjahr: 1984,
+    Wohnort: "USA",
+    Geschlecht: "weiblich",
+    LetzterGespielterFilm: "Black Widow"
+};
+
+const schauspieler = [Johansson, Jackson,WashingtonJr];
+
+console.log(schauspieler);
