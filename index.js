64f4a08d0bfc2d6e6ab5595378336c4ff55fa816
Salimov333 <salimov333@gmail.com>
2021-12-06 12:01:29 +0100

    Salem Lösung

---
+++ index.js
@@ -0,0 +1,24 @@
+const Pitt = { // Brad Pitt
+    Geburtsjahr: 1963,
+    Wohnort: "Oklahoma,",
+    Geschlecht: "Geschlecht",
+    "Letzter gespielte Film": "Irresistible"
+}
+const Hanks = { // Tom Hanks
+    Geburtsjahr: 1956,
+    Wohnort: "Kalifornien",
+    Geschlecht: "männlich",
+    "Letzter gespielte Film": "Finch"
+}
+const Roberts = { // Julia Roberts
+    Geburtsjahr: 1967,
+    Wohnort: "Georgia",
+    Geschlecht: "weiblich",
+    "Letzter gespielte Film": "Ben Is Back"
+}
+const schauspieler = [];
+schauspieler.push(Pitt);
+schauspieler.push(Hanks);
+schauspieler.push(Roberts);
+
+console.log(schauspieler); // in der Konsolenausgabe steht ein Array mit drei Objekten
