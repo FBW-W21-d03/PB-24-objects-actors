8ec71e566b4db5858a668eb927572a2a175605df
sprotte <metatron1972@live.de>
2021-12-06 11:31:08 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,27 @@
+const kermitDerFrosch = {
+    geburtsjahr: 1955,
+    wohnort: "USA",
+    geschlecht: "Froschisch",
+    letzterGespielteFilm:"Muppet Babies" 
+};
+
+const akira = {
+    geburtsjahr: 1988,
+    wohnort: "Japan",
+    geschlecht: "Mänlich",
+    letzterGespielteFilm:"Akira Kinofilm" 
+};
+
+const edwartNorten = {
+    geburtsjahr: 1969,
+    wohnort: "USA",
+    geschlecht: "Mänlich",
+    letzterGespielteFilm:"Das Gesetz der Ehre" 
+};
+
+/*
+console.log(Neviges); 
+console.log("Einwohner:",Neviges.Einwohner);
+console.log("Das Bundesland:",Neviges.Bundesland);
+console.log("Das Gründungsjahr:",Neviges.Gründungsjahr);
+console.log("Sehenswürdigkeiten in Neviges:",Neviges.Sehenswürdigkeiten); */
