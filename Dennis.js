daad23cd1ffe65485fd5f0f7608b11956bc03836
Dennisbullys <d.rochus@hotmail.de>
2021-12-06 11:43:15 +0100

    Dennis

---
+++ Dennis.js
@@ -0,0 +1,34 @@
+// Aufgabenstellung - Teil 1
+
+
+const deniro = {
+    Geburtsjahr : "17.08.1943",
+    Wohnort : "New York",
+    Geschlecht : "Mänlich",
+    LetzterGespielterFilm :"Killers of the Flower Moon"
+};
+
+const alpacino = {
+  Geburtsjahr: "25.04.1940",
+  Wohnort: "New York",
+  Geschlecht: "Mänlich",
+  LetzterGespielterFilm: "House of Gucci",
+};
+
+const reynolds = {
+  Geburtsjahr: "23.10.1976",
+  Wohnort: "Beverly Hills",
+  Geschlecht: "Mänlich",
+  LetzterGespielterFilm:"Red Notice"
+};
+
+// Aufgabenstellung - Teil 2
+
+let schauspieler = ["deniro","alpacino","reynolds"];
+console.log(schauspieler);
+
+
+
+
+
+
