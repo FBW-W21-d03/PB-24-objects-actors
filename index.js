d976a926bd0f24b90a9882ae07b7457a9ee308bf
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-12-06 12:01:01 +0100

    js

---
+++ index.js
@@ -0,0 +1,36 @@
+
+const AngelinaJolie = {
+    Geburtsjahr: 1975,
+    Wohnort:"Los Angeles , Kalifornien" ,
+    Geschlecht: "weiblich",
+    LetzterGespielteFilm: "Eternals"
+};
+
+const BradPitt = {
+    Geburtsjahr: 1963,
+    Wohnort:"Shawnee , Oklahoma" ,
+    Geschlecht: "männlich",
+    LetzterGespielteFilm: "The Big Short"
+};
+
+const AntonioBanderas = {
+    Geburtsjahr: 1960,
+    Wohnort:"Málaga" ,
+    Geschlecht: "männlich",
+    LetzterGespielteFilm: " Killer’s Bodyguard 2"
+};
+
+
+
+console.log("Angelina Jolie:",AngelinaJolie);
+console.log("Geburts jahr:", AngelinaJolie.Geburtsjahr);
+
+
+
+
+let SchauspielerN = ["AngelinaJolie", "AngelinaJolie", "AngelinaJolie"];
+console.log(SchauspielerN);
+
+let Schauspieler = [AngelinaJolie, BradPitt, AntonioBanderas];
+
+console.log(SchauspielerN);
