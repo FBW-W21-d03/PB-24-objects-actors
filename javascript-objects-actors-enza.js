cae704f100b4951908432fa96629444a50fed040
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-12-06 11:11:40 +0100

    06.12.21 aufgabe02

---
+++ javascript-objects-actors-enza.js
@@ -0,0 +1,32 @@
+//Aufgabenstellung - Teil 1
+
+const tappert = {
+    geburtsjahr: 1923 ,
+    wohnort: "München" ,
+    geschlecht: "männlich" ,
+    letzterFilm: "Derrick (Serie)"
+};
+console.log(tappert);
+
+const deNiro = {
+    geburtsjahr: 1943 ,
+    wohnort: "Manhattan" ,
+    geschlecht: "männlich" ,
+    letzterFilm: "Kings of Hollywood"
+};
+console.log(deNiro);
+
+const brandauer = {
+    geburtsjahr: 1943 ,
+    wohnort: "Berlin" ,
+    geschlecht: "männlich" ,
+    letzterFilm: "Ferdinand von Schirach: Feinde (Fernsehfilm)"
+};
+console.log(brandauer);
+
+console.log("----------------");
+
+//Aufgabenstellung - Teil 2
+
+let schauspieler = [tappert , deNiro , brandauer];
+console.log (schauspieler);
