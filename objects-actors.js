2712a052d577088768b1637cbe70a4d1dd83069c
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2021-12-06 11:46:21 +0100

    Orhan's lösung

---
+++ objects-actors.js
@@ -0,0 +1,30 @@
+const tom_cruse = {
+    firsname: 'Tom',
+    lastname: 'Cruse',
+    geburtsjahr: 'July 3, 1962 ',
+    wohnort: 'Syracuse, New York',
+    geshlecht: 'menlich',
+    lezte_geslilte_film: 'Impossible 7'
+}
+
+const brad_pitt = {
+    firsname: 'Brad',
+    lastname: 'Pitt',
+    geburtsjahr: 'December 18, 1963 ',
+    wohnort: 'Shawnee, Oklahoma',
+    geshlecht: 'menlich',
+    lezte_geslilte_film: 'Washington D.C. Area Film Critics Association'
+}
+
+const johnny_depp = {
+    firsname: 'Johnny',
+    lastname: 'Depp',
+    geburtsjahr: 'June 9, 1963',
+    wohnort: 'Owensboro, Kentucky',
+    geshlecht: 'menlich',
+    lezte_geslilte_film: 'The Crimes of Grindelwald'
+}
+
+const schauspieler = [tom_cruse, brad_pitt, johnny_depp];
+
+console.log(schauspieler);
