54f353f6f6791205469b8e61b9c5db2ae4a60490
Serdar68 <Serdarbalkis@outlook.de>
2021-12-06 11:46:41 +0100

    serdar-schauspieler-java

---
+++ index.js
@@ -0,0 +1,23 @@
+const Carray = {
+    Geburtsjahr: "17. Januar 1962",
+    Wohnort: "Los Angeles",
+    Geschlecht: "Männlich",
+    "Letzter Gespielter Film": "Sonic The Hedgehog 2"
+}
+
+const Murphy = {
+    Geburtsjahr: "3. April 1961",
+    Wohnort: "Los Angeles",
+    Geschlecht: "Männlich",
+    "Letzter Gespielter Film": "Der Prinz aus Zamunda 2"
+}
+
+const Reeves = {
+    Geburtsjahr: "2. September 1964",
+    Wohnort: "Los Angeles",
+    Geschlecht: "Männlich",
+    "Letzter Gespielter Film": "Eternals"
+}
+
+let schauspieler = [Carray, Murphy, Reeves]
+console.log(schauspieler)
