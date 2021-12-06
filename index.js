066ec9d572806704bad9497652b3ef9e6cc1a2e2
Mazen <mazen.taha88@gmail.com>
2021-12-06 11:58:59 +0100

    My Lösung

---
+++ index.js
@@ -0,0 +1,26 @@
+const johnnyDepp = {
+    Geburtsjahr:1963,
+    Wohnort:"US",
+    Geschlecht: "männlich",
+    LetzterGespielteFilm: "Minamata",
+};
+
+
+const jimCarrey = {
+    Geburtsjahr:1962,
+    Wohnort:"US",
+    Geschlecht: "männlich",
+    LetzterGespielteFilm: "True Crimes",
+};
+
+
+const willSmith = {
+    Geburtsjahr:1968,
+    Wohnort:"US",
+    Geschlecht: "männlich",
+    LetzterGespielteFilm: "Bad Boys for Life",
+};
+
+let schauspieler = [johnnyDepp, jimCarrey, willSmith];
+
+console.log(schauspieler)
