208fe3b4b60acc4648f45743f5eed76520478594
slavablack <veaceslav.ciornii@magenta.de>
2021-12-06 11:47:31 +0100

    Object 2

---
+++ index.js
@@ -0,0 +1,26 @@
+const JohnnyDeep={
+    geburtsjahr:1963,
+    wohnort: "Owensboro, Kentucky, United States",
+ geschlecht: "mannlich",
+ letztergespieltefilm : "City of Lies"
+    
+}
+
+const AngelinaJolie={
+    geburtsjahr:1975,
+    wohnort: " Los Angeles, California, United States",
+ geschlecht: "weiblich",
+ letztergespieltefilm : "Maleficent: Mistress of Evil"
+    
+}
+
+const WillSmith={
+    geburtsjahr:1968,
+    wohnort: "Philadelphia, Pennsylvania, United States",
+ geschlecht: "mannlich",
+ letztergespieltefilm : "Last Night In Soho"
+    
+}
+
+let schauspiler= [JohnnyDeep,AngelinaJolie,WillSmith]
+console.log(schauspiler)
