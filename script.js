bc4c3018bc0664f4f679589910665111b3a34247
sham912411 <shamalhakim91@gmail.com>
2021-12-06 11:42:11 +0100

    lösung

---
+++ script.js
@@ -0,0 +1,17 @@
+const AdamSandler = {
+    Geburtsjahr : "9. September 1966",
+    Wohnort: " Brooklyn",
+    Geschlecht: "männlich",
+    LetztergespielteFilm: ["Puppy!", "The Week Of	Kenny Lustig","Summer Vacation","The Wrong Missy"]
+  
+  
+};
+
+/*
+Objekte verwenden
+*/
+console.log( AdamSandler)
+console.log (AdamSandler.Geburtsjahr )
+console.log(AdamSandler.Wohnort)
+console.log(AdamSandler.Geschlecht)
+console.log(AdamSandler. LetztergespielteFilm)
