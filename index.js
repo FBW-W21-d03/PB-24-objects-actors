2d44889e667de4fecbee1809049f277a3303022a
aisha2000-hub <aisha.noori2000@gmail.com>
2021-12-06 12:10:30 +0100

    Die Lösung on Aisha

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// TEIL 1
+const leonardo_Dicaprio = {
+      Geburtsjahr :"11 November 1974",
+      Wohnort :"Los Angeles, Kalifornia",
+      Geschlecht :"Männlich",
+      LetzterGespielteFilm :"killers of the flower moon",
+}
+
+const Tom_Hardy = {
+    Geburtsjahr :"15 September 1977",
+    Wohnort :"London",
+    Geschlecht :"Männlich",
+    LetzterGespielteFilm :"The Westland",
+}
+
+const Natalie_Portman = {
+    Geburtsjahr :"09 Juni 1981",
+    Wohnort :"Washington DC",
+    Geschlecht :"Weiblich",
+    LetzterGespielteFilm :"Lucy in the Sky",
+}
+
+
+// Tei 2
+
+let schauspieler =[ "leonardo_Dicaprio" , "Tom_Hardy " , "Natalie_Portman"];
+
+console.log(schauspieler);
