6427f4fa04c4070c3ce868914eccfaff8689b831
Mohal1125 <rajwindersinghmohal@yahoo.com>
2021-12-07 09:04:45 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,27 @@
+const ShahRukhKhan = {
+    Geburtsjahr : "02.11.1965",
+    Wohnort : "Mumbai",
+    Geschlecht : "Mänlich",
+    LetzterGespielterFilm :"Zero"
+};
+
+// Al Pacino
+const IrfanKhan = {
+  Geburtsjahr: "07.01.1967",
+  Wohnort: "Mumbai",
+  Geschlecht: "Mänlich",
+  LetzterGespielterFilm: "Angrezi Medium",
+};
+
+
+const reynolds = {
+  Geburtsjahr: "23.10.1976",
+  Wohnort: "Beverly Hills",
+  Geschlecht: "Mänlich",
+  LetzterGespielterFilm:"Red Notice"
+};
+
+
+
+let schauspielers = [ShahRukhKhan,IrfanKhan,reynolds];
+console.log(schauspielers);
